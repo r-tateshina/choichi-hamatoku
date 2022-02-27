161ea9e5a7c7036d8200c4c9628aac4cf02f1ef5
Ran Tateshina <24566716+r-tateshina@users.noreply.github.com>
2022-02-27 17:44:23 +0900

    Remove unused codes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import pandas as pd
-import numpy as np
 import datetime
 
 app = Flask(__name__)
@@ -12,11 +11,9 @@ def wayback(source):
     today = datetime.datetime.utcnow() + datetime.timedelta(hours=9) - \
         datetime.timedelta(days=1) * 365
     index = source.index[(source["date"]-today).abs().argsort()][0].tolist()
-    print(index)
     tweets = source.loc[index-1:index+1]['url']
     results = []
     for tweet in tweets:
-        print(tweet)
         results.append(tweet)
     return results
 
